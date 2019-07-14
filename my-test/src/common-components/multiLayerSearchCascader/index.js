48ffc2821ab0fc7da1e4e7ff3501d11dcc1331d9
zhangxin63 <zhangxin63@jd.com>
2019-07-14 17:51:31 +0800

    vuezx优化

--- my-test/src/common-components/multiLayerSearchCascader/index.js
+++
@@ -1,9 +0,0 @@
-import MyMultiLayerSearchCascader from './MultiLayerSearchCascader.vue'
-// 注册
-const MultiLayerSearchCascader = {
-  install: (Vue) => {
-    Vue.component('MultiLayerSearchCascader', MyMultiLayerSearchCascader)
-  }
-}
-
-export default MultiLayerSearchCascader
